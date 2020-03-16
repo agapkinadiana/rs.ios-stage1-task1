40edece2981162de0c8dbc97c962f71e8ed2e0b5
agapkinadiana <agapkinadiana@mail.ru>
2020-03-16 18:36:07 +0300

    feat: add solution for 4 task

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.h
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.h
@@ -3,6 +3,7 @@
 @interface Parser : NSObject
 
 - (NSArray <NSString*>*)parseString: (NSString*)string;
+- (NSInteger)getEndIndex:(NSString *)string openBracketType:(char)openBracket startIndex:(NSInteger)index;
 
 @end
 
--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,50 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSMutableArray <NSString*> *resultArray = [NSMutableArray array];
+    
+    for (int i = 0; i < string.length; i++) {
+        char character = [string characterAtIndex:i];
+        
+        if (character == '(' || character == '[' || character == '<') {
+            NSInteger endIndex = [self getEndIndex:string openBracketType:character startIndex:i];
+            if (endIndex > 0) {
+                [resultArray addObject: [string substringWithRange:NSMakeRange(i + 1, endIndex - 1 - i)]];
+            }
+        }
+    }
+    return resultArray;
+}
+
+- (NSInteger)getEndIndex:(NSString *)string openBracketType:(char)openBracket startIndex:(NSInteger)index {
+    NSMutableArray <NSNumber*> *stack= [NSMutableArray array];
+    char closeBracket;
+    
+    if (openBracket == '(') {
+        closeBracket = ')';
+    }
+    else if (openBracket == '[') {
+        closeBracket = ']';
+    }
+    else if (openBracket == '<') {
+        closeBracket = '>';
+    }
+    else return -1;
+    
+    for (NSInteger i = index; i < string.length; i++) {
+        char character = [string characterAtIndex:i];
+        
+        if (character == openBracket) {
+            [stack addObject:@(i)];
+        }
+        else if (character == closeBracket) {
+            [stack removeLastObject];
+            if (stack.count == 0) {
+                return i;
+            }
+        }
+    }
+    return -1;
 }
 
 @end
